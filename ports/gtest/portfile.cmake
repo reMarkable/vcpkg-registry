645263f3bfcf9d7fd27f231b32c2a324efb8188b
Mikhail Svetkin <mikhail.svetkin@gmail.com>
2022-06-13 11:41:37 +0200

    [gtest] new port

---
+++ ports/gtest/portfile.cmake
@@ -0,0 +1,58 @@
+if (EXISTS "${CURRENT_BUILDTREES_DIR}/src/.git")
+    file(REMOVE_RECURSE ${CURRENT_BUILDTREES_DIR}/src)
+endif()
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO google/googletest
+    REF b1f84bf1763b1010597bff13c79b5388eebdf205
+    SHA512 68680a8657898304cbb4f64bfff97e948056484ccc226ff3cadbfeb143dc84b93181157bb652c2fd44e5b48fca3230b39942df9945750f7d0bdf0a794ffa160c
+    HEAD_REF main
+    PATCHES
+        clang-tidy-no-lint.patch
+        fix-main-lib-path.patch
+)
+
+string(COMPARE EQUAL "${VCPKG_CRT_LINKAGE}" "dynamic" GTEST_FORCE_SHARED_CRT)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH ${SOURCE_PATH}
+    OPTIONS
+        -DBUILD_GMOCK=ON
+        -Dgtest_force_shared_crt=${GTEST_FORCE_SHARED_CRT}
+)
+
+vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/GTest)
+
+file(
+    INSTALL
+        "${SOURCE_PATH}/googletest/src/gtest.cc"
+        "${SOURCE_PATH}/googletest/src/gtest_main.cc"
+        "${SOURCE_PATH}/googletest/src/gtest-all.cc"
+        "${SOURCE_PATH}/googletest/src/gtest-death-test.cc"
+        "${SOURCE_PATH}/googletest/src/gtest-filepath.cc"
+        "${SOURCE_PATH}/googletest/src/gtest-internal-inl.h"
+        "${SOURCE_PATH}/googletest/src/gtest-matchers.cc"
+        "${SOURCE_PATH}/googletest/src/gtest-port.cc"
+        "${SOURCE_PATH}/googletest/src/gtest-printers.cc"
+        "${SOURCE_PATH}/googletest/src/gtest-test-part.cc"
+        "${SOURCE_PATH}/googletest/src/gtest-typed-test.cc"
+    DESTINATION
+        ${CURRENT_PACKAGES_DIR}/src
+)
+
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+
+vcpkg_fixup_pkgconfig()
+if(NOT DEFINED VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "debug")
+    vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/lib/pkgconfig/gmock_main.pc" "libdir=\${prefix}/lib" "libdir=\${prefix}/lib/manual-link")
+    vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/lib/pkgconfig/gtest_main.pc" "libdir=\${prefix}/lib" "libdir=\${prefix}/lib/manual-link")
+endif()
+if(NOT DEFINED VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "debug")
+    vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig/gmock_main.pc" "libdir=\${prefix}/lib" "libdir=\${prefix}/lib/manual-link")
+    vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig/gtest_main.pc" "libdir=\${prefix}/lib" "libdir=\${prefix}/lib/manual-link")
+endif()
+vcpkg_copy_pdbs()
+
+file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
